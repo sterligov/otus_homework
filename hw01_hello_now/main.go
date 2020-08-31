4037c7548a017573b7d7711de425f9c8b8d86fa9
Denis Sterligov <denis0324@gmail.com>
2020-08-31 23:42:54 +0500

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const ntpServer = "0.beevik-ntp.pool.ntp.org"
+
 func main() {
-	// Place your code here
+	exactTime, err := ntp.Time(ntpServer)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	currentTime := time.Now()
+
+	fmt.Printf("current time: %s\nexact time: %s\n", currentTime, exactTime)
 }
